55b0e3c5fbf483cd2be2ddfe755192f713a091b9
MariaD-Santos <maria.j.santos108@aluno.senai.br>
2026-08-21 08:16:59 -0300

    ADD: Iniciando importação dos códigos do React para refatoração

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Board from './components/Aero-match/AeroMatchBoard'
 
 function App() {
   
 
   return (
     <>
+    <Board/>
     </>
   )
 }
